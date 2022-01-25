01b0faf98bf25808fcdb6f1fc1bed786eb979cb2
dimadima <44449344+dima69@users.noreply.github.com>
2022-01-25 03:32:43 +0300

    first valuable commit
    first, initial lines of code

---
+++ twitch-chat-username-filter.js
@@ -0,0 +1,96 @@
+let convict_username = '';
+
+// we will scroll back to this position after removing the filter
+let userMessagePosition;
+
+const targetNode = document.querySelector('[data-test-selector="chat-scrollable-area__message-container"]');
+
+// popup window can suck my big black cock
+document.body.querySelector('[data-a-target="chat-user-card"]').style.setProperty('right', '100px');
+document.body.querySelector('[data-a-target="chat-user-card"]').style.setProperty('width', '60%', 'important');
+
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            for (const asd of mutation.addedNodes) {
+                // @@@try other querySelectors
+                if (!asd.querySelector(`[data-a-user="${convict_username}"]`)) {
+                    asd.style.display = 'none';
+                    asd.style.visibility = 'hidden';
+                }
+            }
+        }
+    }
+};
+const observer = new MutationObserver(callback);
+
+const filterButton = document.createElement("button");
+filterButton.setAttribute('data-test-selector', 'username-filter-button');
+filterButton.style.backgroundColor = "#f9f900";
+filterButton.style.fontWeight = "600";
+filterButton.style.padding = "0px 10px";
+filterButton.style.borderRadius = "0.4rem";
+filterButton.style.height = "3rem";
+filterButton.style.alignSelf = "auto";
+
+filterButton.onclick = () => {
+    observer.disconnect();
+    filterButton.remove();
+    // document.body.querySelector('[data-test-selector="username-filter-button"]').remove();
+    const allChatLineMessages = targetNode.querySelectorAll('[data-test-selector="chat-line-message"]');
+    for (const element of allChatLineMessages) {
+        // if there is style attribute in tag = we fucked.
+        // use removeProperty("display") instead? but removing whole attribute should be faster, no?
+        element.removeAttribute('style');
+    }
+    userMessagePosition.scrollIntoView({block: 'center', behavior: 'smooth'});
+    return;
+}
+
+filterButton.onmouseover = () => {
+    filterButton.style.backgroundColor = "#e7e700";
+}
+filterButton.onmouseout = () => {
+    filterButton.style.backgroundColor = "#f9f900";
+}
+
+function addRemoveFilterButton(e) {
+    const chatContainer = document.body.querySelector('[data-test-selector="chat-input-buttons-container"]');
+    const pointsButton = document.body.querySelector('[data-test-selector="community-points-summary"]');
+    filterButton.textContent = `Remove filter for @${convict_username}`;
+    if (pointsButton) {
+        pointsButton.append(filterButton);
+    } else {
+        chatContainer.prepend(filterButton);
+    }
+    // remove text selection, because the nickname is not a <button> but a <span>
+    document.getSelection().removeAllRanges()
+    return;
+}
+
+function userMessagesFilterHandler(event) {
+    // @@@ fix for single click or double click
+    if (event.target.dataset.testSelector === "message-username") {
+        userMessagePosition = event.target;
+        observer.observe(targetNode, config);
+        convict_username = event.target.dataset.aUser;
+        for (let element of targetNode.querySelectorAll('[data-test-selector="chat-line-message"]')) {
+            if (!element.querySelector(`[data-a-user="${convict_username}"]`)) {
+                element.style.display = 'none';
+                element.style.visibility = 'hidden';
+            }
+        }
+        for (let element of targetNode.querySelectorAll('[data-test-selector="user-notice-line"]')) {
+            if (!element.querySelector(`[data-a-user="${convict_username}"]`)) {
+                element.style.display = 'none';
+                element.style.visibility = 'hidden';
+            }
+        }
+        addRemoveFilterButton();
+    }
+}
+
+targetNode.addEventListener('dblclick', userMessagesFilterHandler);
